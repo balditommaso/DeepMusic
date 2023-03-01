0e9f7c85a5d2257251a14769ebefcbc19aca08e2
Tommaso Baldi <84393995+balditommaso@users.noreply.github.com>
2023-03-01 17:37:29 +0100

    Create README.m
    creation of the README file

---
+++ README.m
@@ -0,0 +1,19 @@
+# DeepMusic
+
+Project developed for the Computationa Intelligence and Deep Learning course of the Master of Artificial Intelligence and Data Engineering at the University of Pisa.
+
+This project consists in the design of Deep Learning architecture able to generate both single-track monophonic music, a musical texture with just one voice, and single-track polyphonic music, a type of musical texture consisting of two or more simultaneous lines of independent melody.
+  
+More informetion about the project are available in the documentation.
+
+## Repository
+
+The repository is organized as follows:
+- *Tokenization.ipynb/* contains the implementation of the tokenizer used to process MIDI files
+- *DeepMusic_RNN.ipynb/* contains the implementation of the model based on LSTM
+- *DeepMusic_transformer.ipynb/* contains the implementation of the model based on transformers
+- *models/* contains the trained models.
+
+## Contributors
+- Tommaso Baldi [@balditommaso](https://github.com/balditommaso)
+- Jacopo Cecchetti [@jacopocecch](https://github.com/jacopocecch)
